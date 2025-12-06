b13e07ab5f85714cd393c53c942af2d1c2888457
Maryurit <maryorit@gmail.com>
2025-12-05 22:35:54 -0500

    Frontend conectado al back desplegado

--- src/app/libros/page.js
+++ src/app/libros/page.js
@@ -1,4 +1,6 @@
 'use client';
+export const dynamic = "force-dynamic";
+
 import { useState, useEffect, useCallback } from 'react';
 import { useSearchParams } from 'next/navigation';
 import Navbar from '@/components/Navbar';
